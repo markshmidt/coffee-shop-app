764ec283cf1b125e01862d798ec4502a6366cc11
Mariia <masha.shmidt.04@gmail.com>
2025-08-29 19:35:47 -0400

    migrations applied

--- coffeeshop/store/admin.py
+++ coffeeshop/store/admin.py
@@ -1,3 +1,12 @@
+from _ast import Store
+
 from django.contrib import admin
+from .models import Customer, OrderItem, Order, MenuItem, Category, PaymentRecord
 
 # Register your models here.
+admin.site.register(Customer)
+admin.site.register(OrderItem)
+admin.site.register(Order)
+admin.site.register(MenuItem)
+admin.site.register(Category)
+admin.site.register(PaymentRecord)
