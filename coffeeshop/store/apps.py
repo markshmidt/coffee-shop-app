fa67b65839766ad9b19e0c8ed0051ac521600a09
Mariia <masha.shmidt.04@gmail.com>
2025-08-30 18:40:40 -0400

    manager and barista groups added

--- coffeeshop/store/apps.py
+++ coffeeshop/store/apps.py
@@ -1,6 +1,15 @@
 from django.apps import AppConfig
+from django.contrib.auth.models import Group
+from django.db.models.signals import post_migrate
+from django.dispatch import receiver
 
 
 class StoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'store'
+
+# Adding two groups: barista and manager
+@receiver(post_migrate)
+def ensure_groups(sender, **kwargs):
+    for name in ("BARISTA", "MANAGER"):
+        Group.objects.get_or_create(name=name)
