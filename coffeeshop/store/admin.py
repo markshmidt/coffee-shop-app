3e5ee87fab05760fac2e098b9595ae4961722f27
Mariia <masha.shmidt.04@gmail.com>
2025-08-31 19:13:19 -0400

    model registration and db changes

--- coffeeshop/store/admin.py
+++ coffeeshop/store/admin.py
@@ -1,7 +1,8 @@
 from _ast import Store
 
 from django.contrib import admin
-from .models import Customer, OrderItem, Order, MenuItem, Category, PaymentRecord
+from .models import Customer, OrderItem, Order, MenuItem, Category, PaymentRecord, Variant, ModifierGroup, \
+    ModifierOption, ItemModifierGroup, OrderItemModifier
 
 # Register your models here.
 admin.site.register(Customer)
@@ -10,3 +11,8 @@ admin.site.register(Order)
 admin.site.register(MenuItem)
 admin.site.register(Category)
 admin.site.register(PaymentRecord)
+admin.site.register(Variant)
+admin.site.register(ModifierGroup)
+admin.site.register(ModifierOption)
+admin.site.register(ItemModifierGroup)
+admin.site.register(OrderItemModifier)
