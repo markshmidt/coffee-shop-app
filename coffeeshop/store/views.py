5fd9a4354c7710c27365fe518ecc3e6fd25fa3af
Mariia <masha.shmidt.04@gmail.com>
2025-09-01 15:21:44 -0400

    categories and items added to template

--- coffeeshop/store/views.py
+++ coffeeshop/store/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 
-from .models import Category
+from .models import Category, MenuItem
 
 
 # Create your views here.
 def home(request):
-    categories = Category.objects.prefetch_related("items").all()
-    return render(request, 'home.html', {"categories": categories})
+    parents = (Category.objects
+               .filter(parent__isnull=True)  # only top-level
+               # .prefetch_related("subcategories")
+               .order_by("name")
+               )
+    items = MenuItem.objects.all()
+    return render(request, "home.html", {"categories": parents, "items": items}, )
 
 def api_add_line(request):
     pass
